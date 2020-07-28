d9f7c27c7c6814ff9674e88cefde9339ad1efe2f
Dawley Brady M <DawleyBradyM@JohnDeere.com>
2020-07-28 16:39:48 -0500

    Added Functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,21 +1,34 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[1];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2)
+function checkForMatch()
 {
-	if (cardsInPlay[0] === cardsInPlay[1])
+	if (cardsInPlay.length === 2)
 	{
-		alert("You Found a, match!");
-	}
-	else
-	{
-		alert("Sorry, try again");
+		if (cardsInPlay[0] === cardsInPlay[1])
+		{
+			console.log("You Found a, match!");
+		}
+		else
+		{
+			console.log("Sorry, try again");
+		}
 	}
 }
+
+function flipCard(cardID)
+{
+	cardsInPlay.push(cards[cardID]);
+
+	console.log("User flipped " + cards[cardID]);
+
+	checkForMatch();
+}
+
+flipCard(3);
+flipCard(1);
+
+
+	
+
+	
